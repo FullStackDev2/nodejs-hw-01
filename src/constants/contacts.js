e9330e671b71ec78df51e7b2d5288e3d083cf044
Nurettin Dinçer <imrocker32@gmail.com>
2026-04-14 01:54:36 +0300

    all issues done!

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,16 @@
-export const PATH_DB =
+import path from 'node:path';
+import { faker } from '@faker-js/faker';
+
+export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
+
+
+
+export const createFakeContact = () => {
+  return {
+    id: faker.string.uuid(),
+    name: faker.person.fullName(),
+    phone: faker.phone.number(),
+    email: faker.internet.email(),
+    job: faker.person.jobTitle(),
+  };
+};
